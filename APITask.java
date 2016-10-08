f39991df5ea2b4f7f6100a3e32ea2144f52b2b84
Mitch <mpenney24@googlemail.com>
2016-10-08 15:53:51 +0100

    [Update 03 | MP]
     > Improved caching of data. Now retrieves from cache before requesting from HttpConnection.

--- APITask.java
+++ APITask.java
@@ -1,8 +1,8 @@
 package com.appymitchpenney.hiremitch;
 
+import android.net.http.HttpResponseCache;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -10,7 +10,10 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.CacheResponse;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.sql.Timestamp;
 import java.text.ParseException;
@@ -18,42 +21,59 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 public class APITask extends AsyncTask {
+    private final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'");
+    private final int UNPROCESSABLE_ENTITY = 422;
     private HashSet jsons = new HashSet();
-    private final SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'");
 
     @Override
     protected Object doInBackground(Object[] objects) {
+        URL url = (URL) objects[0];
+        URI uri = null;
         HttpURLConnection con = null;
-        StringBuilder result = new StringBuilder();
+        CacheResponse cache;
 
         try {
-            int current;
-            URL url = (URL) objects[0];
-            con = (HttpURLConnection) url.openConnection();
-            con.setUseCaches(true);
-            InputStream in = con.getInputStream();
-
-            current = in.read();
-
-            while(current != -1) {
-                char letter = (char) current;
-                result.append(letter);
-                current = in.read();
-            }
-
-            return result;
-
-        } catch (IOException e) {
-            Log.e("ERROR:APITask",e.getMessage());
+            uri = new URI(url.toString());
+        } catch (URISyntaxException e) {
             e.printStackTrace();
-        } finally {
-            if(con != null) {
-                con.disconnect();
-            }
+            Log.e("ERR","URI could not be created from URL!");
         }
 
+        if(uri != null) {
+            try {
+                con = (HttpURLConnection) url.openConnection();
+                con.setUseCaches(true);
+
+                if(HttpResponseCache.getInstalled() != null) {
+                    cache = HttpResponseCache.getInstalled().get(uri, "GET", con.getRequestProperties());
+
+                    if (cache != null) {
+                        Log.i("INF","Reading from cache!");
+                        return readData(cache.getBody());
+                    }
+                }
+
+                if (con.getResponseCode() == UNPROCESSABLE_ENTITY) {
+                    throw new IOException();
+                } else if (con.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    throw new IOException();
+                } else {
+                    Log.i("INF","Reading from HttpConnection!");
+                    return readData(con.getInputStream());
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+                Log.e("ERR","HTTP Response was NOT OK!");
+            } finally {
+                if (con != null) {
+                    con.disconnect();
+                }
+            }
+        }
         return null;
     }
 
@@ -61,31 +81,74 @@ public class APITask extends AsyncTask {
     protected void onPostExecute(Object o) {
         super.onPostExecute(o);
 
-        try {
-            JSONArray arr = new JSONArray(o.toString());
+        if (o != null) {
+            try {
+                JSONArray arr = new JSONArray(o.toString());
 
-            for(int i = 0; i < arr.length(); i++) {
-                JSONObject obj = arr.getJSONObject(i);
-                HashMap<String,String> map = new HashMap<>();
+                for (int i = 0; i < arr.length(); i++) {
+                    JSONObject obj = arr.getJSONObject(i);
+                    HashMap<String, String> map = new HashMap<>();
 
-                Date created = fmt.parse(obj.getString("created_at"));
-                Date updated = fmt.parse(obj.getString("updated_at"));
+                    Date created = FORMAT.parse(obj.getString("created_at"));
+                    Date updated = FORMAT.parse(obj.getString("updated_at"));
 
-                map.put("id",obj.getString("id"));
-                map.put("name",obj.getString("name"));
-                map.put("start",obj.getString("start"));
-                map.put("end",obj.getString("end"));
-                map.put("created_at",new Timestamp(created.getTime()).toString());
-                map.put("updated_at",new Timestamp(updated.getTime()).toString());
+                    map.put("id", obj.getString("id"));
+                    map.put("name", obj.getString("name"));
+                    map.put("start", obj.getString("start"));
+                    map.put("end", obj.getString("end"));
+                    map.put("created_at", new Timestamp(created.getTime()).toString());
+                    map.put("updated_at", new Timestamp(updated.getTime()).toString());
 
-                jsons.add(map);
-                Log.i("DATA",obj.getString("id"));
+                    jsons.add(map);
+                    Log.i("DATA", obj.getString("id"));
+                }
+            } catch (JSONException | ParseException e) {
+                e.printStackTrace();
+                Log.e("ERROR", "JSON conversion failed!");
             }
-        } catch (JSONException|ParseException e) {
+
+            MainActivity.txtWorld.setText(jsons.toString());
+        }
+    }
+
+    private String readData(InputStream in) {
+        StringBuilder result = new StringBuilder();
+        int current = 0;
+
+        try {
+            current = in.read();
+
+            while (current != -1) {
+                char letter = (char) current;
+                result.append(letter);
+                current = in.read();
+            }
+
+            return result.toString();
+        } catch (IOException e) {
             e.printStackTrace();
-            Log.e("ERROR","JSON conversion failed!");
+            Log.e("ERR","Data read failed!");
         }
 
-        MainActivity.txtWorld.setText(jsons.toString());
+        return null;
     }
 }
+
+//Long expires = con.getHeaderFieldDate("Expires",System.currentTimeMillis());
+//con.setUseCaches(true);
+
+/*if(HttpResponseCache.getInstalled() != null) {
+        cache = HttpResponseCache.getInstalled().get(uri,"GET",con.getRequestProperties());
+        if (cache != null) {
+        Map<String,List<String>> headers = cache.getHeaders();
+        if (headers.containsKey("Last-Modified")) {
+        List<String> modifyList = headers.get("Last-Modified");
+        if (modifyList != null && !modifyList.isEmpty()) {
+        String modified = modifyList.get(0);
+        if (modified != null) {
+        con.addRequestProperty("If-Modified-Since",modified);
+        }
+        }
+        }
+        }
+        }*/
