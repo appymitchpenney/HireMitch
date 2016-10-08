efc33d54fb95020058fdc4d865dea513258a6548
Mitch <mpenney24@googlemail.com>
2016-10-08 12:57:02 +0100

    [Update 02 | MP]
     > Got HttpURLConnection working for given API.

--- APITask.java
+++ APITask.java
@@ -2,12 +2,26 @@ package com.appymitchpenney.hiremitch;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class APITask extends AsyncTask {
+    private HashSet jsons = new HashSet();
+    private final SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'");
 
     @Override
     protected Object doInBackground(Object[] objects) {
@@ -29,9 +43,9 @@ public class APITask extends AsyncTask {
                 current = in.read();
             }
 
-            return result.toString();
+            return result;
 
-        } catch (Exception e) {
+        } catch (IOException e) {
             Log.e("ERROR:APITask",e.getMessage());
             e.printStackTrace();
         } finally {
@@ -41,5 +55,37 @@ public class APITask extends AsyncTask {
         }
 
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+
+        try {
+            JSONArray arr = new JSONArray(o.toString());
+
+            for(int i = 0; i < arr.length(); i++) {
+                JSONObject obj = arr.getJSONObject(i);
+                HashMap<String,String> map = new HashMap<>();
+
+                Date created = fmt.parse(obj.getString("created_at"));
+                Date updated = fmt.parse(obj.getString("updated_at"));
+
+                map.put("id",obj.getString("id"));
+                map.put("name",obj.getString("name"));
+                map.put("start",obj.getString("start"));
+                map.put("end",obj.getString("end"));
+                map.put("created_at",new Timestamp(created.getTime()).toString());
+                map.put("updated_at",new Timestamp(updated.getTime()).toString());
+
+                jsons.add(map);
+                Log.i("DATA",obj.getString("id"));
+            }
+        } catch (JSONException|ParseException e) {
+            e.printStackTrace();
+            Log.e("ERROR","JSON conversion failed!");
+        }
+
+        MainActivity.txtWorld.setText(jsons.toString());
     }
 }
--- MainActivity.java
+++ MainActivity.java
@@ -1,21 +1,27 @@
 package com.appymitchpenney.hiremitch;
 
-import android.content.Context;
 import android.net.http.HttpResponseCache;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    public static TextView txtWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtWorld = (TextView) findViewById(R.id.txtWorld);
 
         try {
             File httpCacheDir = new File(this.getCacheDir(), "http");
@@ -26,10 +32,21 @@ public class MainActivity extends AppCompatActivity {
             Log.e("ERROR", "Cache could not be created!");
         }
 
+        try {
+            URL url = new URL("http://shrouded-woodland-9458.herokuapp.com/events");
+            APITask task = new APITask();
+            task.execute(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            Log.e("ERROR","URL was invalid!");
+            this.finish();
+        }
+
     }
 
     @Override
     protected void onStop() {
+        super.onStop();
         HttpResponseCache cache = HttpResponseCache.getInstalled();
         Log.i("INF","Cache flush?");
         if (cache != null) {
